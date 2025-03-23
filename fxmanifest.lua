a10df0774c973ff2fb0534a955c6d8ee6df34550
Rico® <177413802+realricos@users.noreply.github.com>
2025-03-23 01:13:56 +0100

    .

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Rico'
+description 'Commands script for QBCore Framework'
+version '1.0.0'
+
+client_scripts {
+     'client/commands.lua',
+     'client/noclip.lua',
+    --'client/spawncar.lua',  -- uncomment if your using qs-vehiclekeys !
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
